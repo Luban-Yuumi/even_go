7c7e8e482ff72c04a9ebcafb3ad3cbf8f78da9c7
nshu <1850816020@qq.com>
2018-09-17 23:37:04 +0800

    管道

--- chan/chan.go
+++ chan/chan.go
@@ -6,14 +6,19 @@ import (
 )
 
 func main() {
+	a := 3
 	c := make(chan int, 3)
-	c <- 3
+	c <- a
 	c <- 2
 	c <- 1
-	close(c)
+	close(c) //如果不关闭通道，会引发daedlock
 	for {
 		if value, ok := <-c; ok {
+			if value == 3 {
+				value = 4
+			}
 			fmt.Println(value)
+			fmt.Println(a)
 		} else {
 			os.Exit(0)
 		}
